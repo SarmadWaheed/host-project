e4963f165c8f0aa3cfe27bb4bf50214aa169e772
SARMAD <sarmadwaheed157@gmail.com>
2024-01-20 13:08:23 +0500

    com

--- router/admin-router.js
+++ router/admin-router.js
@@ -4,12 +4,12 @@ const authadmin=require('../controller/admin-controller');
 const authmiddleware=require('../middleware/authmiddleware');
 const adminmiddleware=require('../middleware/admin-middleware');
 
-router.get('/user',authmiddleware,adminmiddleware,authadmin.getusersdata);
-router.get('/user/:id',authmiddleware,adminmiddleware,authadmin.getsingleuserdata);
-router.get('/contact',authmiddleware,adminmiddleware,authadmin.getcontactdata);
-router.patch('/user/update/:id',authmiddleware,adminmiddleware,authadmin.updatesingleuserdata)
-router.delete('/user/delete/:id',authmiddleware,adminmiddleware,authadmin.deleteuserdata);
-router.delete('/contact/delete/:id',authmiddleware,adminmiddleware,authadmin.deletecontactdata);
+router.get('/user',authmiddleware,authadmin.getusersdata);
+router.get('/user/:id',authmiddleware,authadmin.getsingleuserdata);
+router.get('/contact',authmiddleware,authadmin.getcontactdata);
+router.patch('/user/update/:id',authmiddleware,authadmin.updatesingleuserdata)
+router.delete('/user/delete/:id',authmiddleware,authadmin.deleteuserdata);
+router.delete('/contact/delete/:id',authmiddleware,authadmin.deletecontactdata);
     
 
 
